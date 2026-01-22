6021ea453cf852b1a808f5b9887672536b9cfc6d
park8749 <parks8749@gmail.com>
2026-01-22 01:14:00 -0800

    renamed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FarLeave.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FarLeave.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name="Leave", group="Autonomous")
-public class Leave extends LinearOpMode
+@Autonomous(name="FarLeave", group="Autonomous")
+public class FarLeave extends LinearOpMode
 {
     Driver driver;
     public CRServo backBottom;
