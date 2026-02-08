1c3601e3dd01c4da0c646cde62ea0c4d1c088e52
Rugvedhs <Rugvedhs@gmail.com>
2026-02-08 13:51:17 -0800

    Added limelight configurations!!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTag21.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@Autonomous(name = "AprilTag21")
+public class AprilTag21 extends LinearOpMode {
+    Driver driver;
+    public CRServo backBottom;
+    public CRServo backIntake;
+    public CRServo launcherWheel;
+    public DcMotor leftFlyWheel;
+    public DcMotor rightFlyWheel;
+    public CRServo rightBelt;
+    public CRServo leftBelt;
+    public CRServo frontIntake;
+
+
+    @Override
+    public void runOpMode() {
+        driver = new Driver(this, hardwareMap);
+        backBottom   = (hardwareMap.get(CRServo.class, "BackBottom"));
+        backIntake   = (hardwareMap.get(CRServo.class, "BackIntake"));
+        launcherWheel= (hardwareMap.get(CRServo.class, "LauncherWheel"));
+        leftFlyWheel = (hardwareMap.get(DcMotor.class, "leftFly"));
+        rightFlyWheel = (hardwareMap.get(DcMotor.class, "rightFly"));
+        leftBelt = (hardwareMap.get(CRServo.class, "LeftBelt"));
+        rightBelt = (hardwareMap.get(CRServo.class, "RightBelt"));
+        frontIntake = (hardwareMap.get(CRServo.class, "FrontIntake"));
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        waitForStart();
+
+        if (opModeIsActive())
+        {
+            // turn off limelight camera, because it overheats and uses too much energy
+            // the first half of this where it goes back ready to almost turn before moves forward
+            driver.forward_tiles(-3.5);
+            driver.turn_ticks(-90,1); // fix this
+            driver.forward_tiles(2.3);
+            frontIntake.setPower(1.0);
+            rightBelt.setPower(1.0);
+            leftBelt.setPower(-1.0);
+            launcherWheel.setPower(1.0);
+            backIntake.setPower(-1.0);
+            sleep(500);
+//-------------------------------------------------------------------------------
+            driver.forward_tiles(-1);
+            driver.strafe_tiles(3.5);
+            driver.forward_tiles(1);
+            driver.turn_ticks(30,1);
+            //shoot artifacts
+            rightBelt.setPower(1.0);
+            leftBelt.setPower(-1.0);
+            backBottom.setPower(1.0);
+            launcherWheel.setPower(1.0);
+            backIntake.setPower(-1.0);
+            leftFlyWheel.setPower(-1.0);
+            rightFlyWheel.setPower(1.0);
+            sleep(4000);
+//-------------------------------------------------------------------------------
+            // part c
+            driver.forward_tiles(-1);
+            driver.turn_ticks(90,1);
+            driver.strafe_tiles(-1);
+            driver.forward_tiles(2.3);
+
+            // pick up
+            frontIntake.setPower(1.0);
+            rightBelt.setPower(1.0);
+            leftBelt.setPower(-1.0);
+            launcherWheel.setPower(1.0);
+            backIntake.setPower(-1.0);
+            sleep(500);
+
+            // ready to shoot
+            driver.strafe_tiles(2.5);
+            driver.forward_tiles(1);
+            driver.turn_ticks(30,1);
+
+            //shoot artifacts
+            rightBelt.setPower(1.0);
+            leftBelt.setPower(-1.0);
+            backBottom.setPower(1.0);
+            launcherWheel.setPower(1.0);
+            backIntake.setPower(-1.0);
+            leftFlyWheel.setPower(-1.0);
+            rightFlyWheel.setPower(1.0);
+            sleep(4000);
+
+            //strafes out
+            rightBelt.setPower(0);
+            leftBelt.setPower(0);
+            backBottom.setPower(0);
+            launcherWheel.setPower(0);
+            backIntake.setPower(0);
+            leftFlyWheel.setPower(0.0);
+            rightFlyWheel.setPower(0.0);
+            driver.strafe_tiles(1.1,1);
+
+
+        }
+
+    }
+}
