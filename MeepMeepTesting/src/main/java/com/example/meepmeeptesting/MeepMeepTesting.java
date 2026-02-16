8b1675847b6dcb59b047ffe826b43b90142b3b6c
park8749 <parks8749@gmail.com>
2026-02-15 17:48:22 -0800

    idk

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,24 +17,49 @@ public class MeepMeepTesting {
 
 
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-53, -47, Math.toRadians(232)))
-                .strafeTo(new Vector2d(-46,-39))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-47, 0, Math.toRadians(232)))
 
-                .strafeToLinearHeading(new Vector2d(-11.5,-22), Math.toRadians(272))
-                .strafeTo(new Vector2d(-11.5,-55))
-                .strafeToLinearHeading(new Vector2d(-46,-39), Math.toRadians(232))
-
-//                .strafeToLinearHeading(new Vector2d(12, -26), Math.toRadians(274))
-//                .stopAndAdd(intakeStack())
-//                .strafeTo(new Vector2d(12,-60))
-//                .stopAndAdd(stopIntake())
-//                .strafeTo(new Vector2d(12,-50))
-//                .strafeToLinearHeading(new Vector2d(-46,-39), Math.toRadians(232))
-//                .stopAndAdd(shootFrontIntake())
-//                .waitSeconds(3)
-//                .stopAndAdd(stopAll())
-
+                .strafeToLinearHeading(new Vector2d(9,-20),Math.toRadians(230))
+                .strafeToLinearHeading(new Vector2d(12,-27),Math.toRadians(270))
+                .strafeTo(new Vector2d(12,-54))
+                .strafeTo(new Vector2d(12,-27))
+                .strafeTo(new Vector2d(-48,-29))
+                .strafeToLinearHeading(new Vector2d(-47,-47), Math.toRadians(230))
+                .strafeToLinearHeading(new Vector2d(-10,-19), Math.toRadians(272))
+                .strafeTo(new Vector2d(-12,-55))
+                .strafeToLinearHeading(new Vector2d(-47,-47), Math.toRadians(230))
                 .strafeTo(new Vector2d(-60,-33))
+
+
+//                .strafeToLinearHeading(new Vector2d(35,-26), Math.toRadians(272))
+//                .strafeTo(new Vector2d(35,-55))
+//                .strafeTo(new Vector2d(35,-45))
+//                .splineToLinearHeading(new Pose2d(-48,-38,Math.toRadians(232)), Math.toRadians(240))
+//                .strafeToLinearHeading(new Vector2d(-11.5, -22), Math.toRadians(272))
+//                .strafeTo(new Vector2d(-11.5,-53))
+//                .strafeToLinearHeading(new Vector2d(-48,-38), Math.toRadians(232))
+//                .strafeTo(new Vector2d(-58,-35))
+
+
+
+
+
+
+
+                // starting position and heading for close auto
+                // (-53, -47, Math.toRadians(232))
+                // (x, y, heading) for first spike mark
+                // (-11.5, -22, Math.toRadians(272))
+                // (x, y, heading) for second spike mark
+                // (12, -26, Math.toRadians(272))
+                // (x, y, heading) for third spike mark
+                // (35, -26, Math.toRadians(272))
+
+                // starting position and heading for far auto
+                // (62, -16, Math.toRadians(180))
+
+
+
                 .build());
 
 
