74eb8eadf73f19766d3b0e5cb42c13231992e1e6
Rugvedhs <Rugvedhs@gmail.com>
2026-03-01 19:32:35 -0800

    All changed april tag files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagTestRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagTestRed.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name="AprilTagTestRed", group="Autonomous")
